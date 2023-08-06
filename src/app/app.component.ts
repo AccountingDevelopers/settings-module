bfcbe768a756db779b5568af5d5f9c34e1434eaf
unknown <millerkirillmail@gmail.com>
2023-08-06 15:35:17 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { AccSystemService } from 'ng-accounting'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'settings-module';
+export class AppComponent implements OnInit {
+  constructor(private readonly accSystemService: AccSystemService) { }
+
+  stepActiveIndex: number = 0
+  registerFieldsOptions: any = {}
+  users: any[] = []
+  mainSettingsModel: MenuItem[] = [
+    {
+      label: 'Головне'
+    },
+    {
+      label: 'Коди'
+    },
+    {
+      label: 'Реєстрація'
+    },
+    {
+      label: 'Адреси'
+    }
+  ]
+
+  ngOnInit(): void {
+    this.users = this.accSystemService.users
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabViewModule } from 'primeng/tabview';
 import { AppComponent } from './app.component';
+import { StepsModule } from 'primeng/steps';
+import { DropdownModule } from 'primeng/dropdown';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
+import { TreeSelectModule } from 'primeng/treeselect';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { NgAccountingModule } from 'ng-accounting';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    TabViewModule,
+    StepsModule,
+    DropdownModule,
+    TreeSelectModule,
+    DividerModule,
+    CalendarModule,
+    InputTextModule,
+    NgAccountingModule.forRoot({
+      api: {
+        key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGFlZGY2YmRmMWRhOTkwZWI0MmI1YjQiLCJjb21wYW55SWQiOiI2NGFmZmQ4ZDhlYjFkMzJhODBlM2I0YWIiLCJ3b3Jrc3BhY2VOYW1lIjoiam91cm5leSIsImlhdCI6MTY5MDU2MjAxMSwiZXhwIjoxNzE2NDgyMDExfQ.BRZ-3OIJ4qMpoH0CbEFr05mHfQKV4r1nZlKOuWiXv8U',
+        url: {
+          client: 'http://localhost:4200',
+          server: isDevMode() ? 'http://localhost:80/api/v1' : 'http://localhost:80/api/v1'
+        }
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
